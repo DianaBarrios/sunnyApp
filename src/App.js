bc2ae45889aa9596b2c1098fe8650724f761eb7e
Diana Barrios <dianabarriosmacias@hotmail.com>
2019-06-17 13:43:14 +0200

    first files

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          This will be the landing page.
+        </p>
+        <p>
+          This will be the link to the project description.
+        </p>
+        <p>
+          This will be the link to create a project.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
