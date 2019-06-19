d54178fef56011bf5bc55e5ebee18b41f0be231a
Luis Kentzler <kentzler@Kentzlers-MacBook-Pro.local>
2019-06-19 15:54:18 +0200

    Styled the header

--- src/App.js
+++ src/App.js
@@ -9,12 +9,19 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p class="text-primary">
-            This is the landing page.
-        </p>
-        </header>
-
+        <div class ="wrapper">
+          <div class = "logo py-4 px-4">
+            <img src="https://firebasestorage.googleapis.com/v0/b/smiles-ai-images/o/logo_smile.ai.jpg?alt=media&token=ea199bd3-4c7c-47fe-9a58-7c9efd054ce4" class="float-left" alt="Our logo"/>
+          </div>
+          <div className="App-header body-content">
+            <p class="text2">
+              The Easiest Way to Help <br/>your Community
+            </p>
+            <p class="text-secondary">
+              You have the opportunity to engage in CSR <br/>activities very easy with our support.
+            </p>
+          </div>
+        </div>
         <div class="container my-5">
           <h1>Join a project</h1>
           <ShowProjects />
