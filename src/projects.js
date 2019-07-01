53b980dfcf084b853e30cdd83877164a0900b50a
Diana Barrios <dianabarriosmacias@hotmail.com>
2019-07-01 17:30:08 +0200

    fixed list of projects

--- src/projects.js
+++ src/projects.js
@@ -16,7 +16,7 @@ class Projects extends React.Component {
   }
 
   componentDidMount() {
-    let projectsRef = db.collection("projects").orderBy("projectName");
+    let projectsRef = db.collection("projects").orderBy('projectName');
 
     let query = projectsRef.get().then(snapshot => {
       snapshot.docs.forEach(doc => {
@@ -31,18 +31,30 @@ class Projects extends React.Component {
     const { url } = this.props.match;
     return (
       <div>
-        <h1>Projects</h1>
-        <strong>Select a project</strong>
-        <ol>
-          {this.state.docs.map(doc => (
-            <li>
-              <Link to={`projects/${doc.segmentID}`} key={doc.id}>
-                {" "}
-                {doc.projectName}{" "}
-              </Link>
-            </li>
-          ))}
-        </ol>
+
+        <div class="container">
+          <h1>Projects</h1>
+          <strong>Select a project</strong>
+        </div>
+        
+        <div class="container">
+          <ul class="list-unstyled">
+              {this.state.docs.map(doc => (
+                <li class="media my-3">
+                  <img src="https://www.xing.com/image/c_2_b_d6d996c21_22325856_4/thomas-stanner-foto.256x256.jpg" class="align-self-center mr-3" alt="..."></img>
+                  <div class="media-body">
+                    <Link to={`projects/${doc.segmentID}`} key={doc.id}> 
+                    <h5 class="mt-0 mb-1"> {doc.projectName} </h5>
+                    </Link>
+                    <p>Contact: {doc.email}</p>
+                    <p>Organizer: {doc.firstName} {doc.lastName}</p>
+                    <p>Description: {doc.description}</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+        </div>  
+
       </div>
     );
   }
