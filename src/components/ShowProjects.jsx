7672ccc5b4ea3caf399c8b090b56cff8ea6374cb
Diana Barrios <dianabarriosmacias@hotmail.com>
2019-07-01 11:01:25 +0200

    fixed link in button

--- src/components/ShowProjects.jsx
+++ src/components/ShowProjects.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { from } from "rxjs";
+import { Route, Link } from "react-router-dom";
 const firebase = require("../firebase.js");
 const db = firebase.db;
 
@@ -39,9 +40,9 @@ class ShowProjects extends React.Component {
                             <div class="card-body">
                                 <h5 class="card-title" key={doc.id}>{doc.projectName}</h5>
                                 <p class="card-text" key={doc.id}> {doc.description}</p>
-                                <a href="" class="btn btn-primary">
-                                    Read more
-                    </a>
+                                <Link to={`projects/${doc.segmentID}`} key={doc.id}>
+                                <button type="button" class="btn btn-primary">Read more</button>
+                                </Link>
                             </div>
                         </div>
                     ))}
