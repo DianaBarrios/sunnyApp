f53caf2aa0f1cb8ff3be33a3b06039f5e4a6472c
lustoykov <lustoykov@gmail.com>
2019-07-03 21:54:37 +0200

    Home design

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { useContext } from "react";
-import "./App.css";
+import styles from "./App.module.css";
 import Signup from "./Signup";
 import Signin from "./Signin";
 import ShowProjects from "./components/ShowProjects";
+import bubbleSrc from "./bubble.svg";
+import peopleSrc from "./bubble-people.svg";
+import joinProjectSrc from "./join-project.svg";
+import proposeProjectSrc from "./propose-project.svg";
 import { Link } from "react-router-dom";
 import { UserAuthContext } from "./UserProvider";
 import { Redirect } from "react-router-dom";
@@ -15,90 +19,61 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <div className="wrapper">
-        <div className="App-header body-content">
-          <Signup />
-          <Signin />
-          <p className="text2">
-            The Easiest Way to Help <br />
-            your Community
-          </p>
-          <p className="text-secondary">
-            You have the opportunity to engage in CSR <br />
-            activities very easy with our support.
-          </p>
-        </div>
-      </div>
-
-      <div className="container my-5">
-        <div className="container">
-          <h1>What you can do with Corthropy </h1>
-        </div>
-
-        <div className="row">
-          <div className="col">
-            <h2>Join a project</h2>
-            <p>
-              Choose from a huge selection and find exactly the project that
-              really suits you!
-            </p>
+    <>
+      <div className={styles.content}>
+        <header className={styles.header}>
+          <Link to="/">
+            <img
+              className={styles.logo}
+              src="https://firebasestorage.googleapis.com/v0/b/smiles-ai-images/o/logo_smile.ai.jpg?alt=media&token=ea199bd3-4c7c-47fe-9a58-7c9efd054ce4"
+              alt="Our logo"
+            />
+          </Link>
+          <div className={styles.auth}>
+            <a> Sign in </a>
+            <a> Sign up </a>
           </div>
-          <div className="col">
-            <h2>Propose a project</h2>
-            <p>
-              Do you have some cool idea that you want to support? Tell us more!
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div className="App-join py-4">
-        <p className="h1">Join a project</p>
-
-        <div className="container my-3">
-          <ShowProjects />
+        </header>
+        <div className={styles.pushContent} />
+        <div>
+          <img src={bubbleSrc} className={styles.bubble} />
+          <img src={peopleSrc} className={styles.people} />
         </div>
 
-        <Link to="/projects">See more projects</Link>
-      </div>
-
-      <div className="App-section container my-5">
-        <h1>Propose a project</h1>
-
-        <div className="container mt-3 mb-5">
+        <section className={styles.slogan}>
+          <h1> We provide the time.</h1>
+          <h1> You provide the hands.</h1>
           <p>
-            We all want to change the world! Do you know about an existing
-            project that you want to support or have one of your own? Tell us
-            about your project and we'll help you make it a reality!
+            Corthropy wants to make an impact. You too. Let’s do it! Choose from
+            the available projects and we’ll give you time off your work week to
+            participate. It’s never been easier.
           </p>
-        </div>
+        </section>
 
-        <Link to="/project-proposal">
-          <button type="button" className="btn btn-primary btn-lg">
-            Start
-          </button>
-        </Link>
+        <section className={styles.pitch}>
+          <h1> What you can do with Corthropy & Fond Finaz </h1>
+          <div className={styles.offers}>
+            <div className={styles.offer}>
+              <img src={joinProjectSrc} />
+              <h3> Join an existing project </h3>
+              <p>
+                Choose from a huge selection and find exactly the project that
+                really suits you
+              </p>
+            </div>
+            <div className={styles.offer}>
+              <img src={proposeProjectSrc} />
+              <h3> Propose project </h3>
+              <p>
+                Do you know of a project that you want to support? Tell us more!
+              </p>
+            </div>
+          </div>
+        </section>
       </div>
 
-      <div className="App-footer container-fluid py-4 pl-4">
-        <div className="d-flex flex-row bd-highlight mb-3 text-nav-footer">
-          <div className="p-2 bd-highlight">About</div>
-          <a href="https://try.corthropy.com/" className="p-2 bd-highlight">
-            Product
-          </a>
-        </div>
-
-        <div className="d-flex bd-highlight">
-          <div className="mr-auto p-2 bd-highlight">
-            2019 All rights reserved
-          </div>
-          <div className="ml-auto p-2 bd-highlight">
-            Made with love by Smile.AI
-          </div>
-        </div>
-      </div>
-    </div>
+      <footer className={styles.footer} />
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { Route, Link, BrowserRouter as Router, Switch } from "react-router-dom";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import App from "./App";
 import Projects from "./projects";
 import ProjectDescription from "./project-description";
@@ -14,26 +14,15 @@ import ProtectedRoute from "./ProtectedRoute";
 const routing = (
   <UserProvider>
     <Router>
-      <div>
-        <Link to="/">
-          <div className="logo py-4 px-4 mb-4">
-            <img
-              src="https://firebasestorage.googleapis.com/v0/b/smiles-ai-images/o/logo_smile.ai.jpg?alt=media&token=ea199bd3-4c7c-47fe-9a58-7c9efd054ce4"
-              className="float-left"
-              alt="Our logo"
-            />
-          </div>
-        </Link>
-        <Switch>
-          <Route exact path="/" component={App} />
-          <ProtectedRoute>
-            <Route exact path="/projects" component={Projects} />
-            <Route exact path="/projects/:id" component={ProjectDescription} />
-            <Route exact path="/project-proposal" component={ProjectProposal} />
-          </ProtectedRoute>
-          <Route component={Notfound} />
-        </Switch>
-      </div>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <ProtectedRoute>
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/projects/:id" component={ProjectDescription} />
+          <Route exact path="/project-proposal" component={ProjectProposal} />
+        </ProtectedRoute>
+        <Route component={Notfound} />
+      </Switch>
     </Router>
   </UserProvider>
 );
