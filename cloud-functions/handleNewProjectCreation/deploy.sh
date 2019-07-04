139ba899b3c5a78a825b6b089ad01f3beee8b6ac
lustoykov <lustoykov@gmail.com>
2019-07-04 11:25:43 +0200

    Change Cloud Func Config

--- cloud-functions/handleNewProjectCreation/deploy.sh
+++ cloud-functions/handleNewProjectCreation/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 gcloud functions deploy handleNewProjectCreation \
   --runtime nodejs10 \
-  --trigger-resource projects/smiles-ai/databases/smiles-ai/documents/projects/{id} \
+  --trigger-resource "projects/smiles-ai/databases/(default)/documents/projects/{id}" \
   --trigger-event providers/cloud.firestore/eventTypes/document.create \
   --env-vars-file ../.env.yaml \
--- cloud-functions/subscribeCreatorToProject/deploy.sh
+++ cloud-functions/subscribeCreatorToProject/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 gcloud functions deploy subscribeCreatorToProject \
   --runtime nodejs10 \
-  --trigger-resource projects/smiles-ai/databases/smiles-ai/documents/projects/{id} \
+  --trigger-resource "projects/smiles-ai/databases/(default)/documents/projects/{id}" \
   --trigger-event providers/cloud.firestore/eventTypes/document.update \
   --env-vars-file ../.env.yaml \
