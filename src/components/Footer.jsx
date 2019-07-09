fe24d52b46bb3d6bce2a63115d9714751f8385de
lustoykov <lustoykov@gmail.com>
2019-07-09 15:02:42 +0200

    Fix footer logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,28 @@
 import React, { useContext, useState, useEffect } from "react";
 import styles from "./Footer.module.css";
+import logoWhiteSrc from "./logo_white.png";
 
 function Footer() {
-
   return (
     <>
       <footer className={styles.nav}>
         <ul>
-          <li><a href="http://www.corthropy.com">About</a></li>
-          <li><a href="http://www.corthropy.com">Product</a></li>
-          <li><a href="http://www.corthropy.com">Team</a></li>
+          <li>
+            <a href="http://www.corthropy.com">About</a>
+          </li>
+          <li>
+            <a href="http://www.corthropy.com">Product</a>
+          </li>
+          <li>
+            <a href="http://www.corthropy.com">Team</a>
+          </li>
         </ul>
-        <img src="logo_white.png" alt="Corthropy logo"/>
+        <img src={logoWhiteSrc} alt="Corthropy logo" />
 
         <p>2019 All rights reserved</p>
-        <p>Made with <span>♥</span> by Corthropy</p>
-
-
+        <p>
+          Made with <span>♥</span> by Corthropy
+        </p>
       </footer>
     </>
   );
